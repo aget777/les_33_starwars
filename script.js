5ec74421636d0b0c3111d76ddb7615b88bd7b82a
Alex <am2invest@yandex.ru>
2021-01-26 16:07:00 +0500

    добавил фунцию, которая заполняет карточку данными героя

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
 let dataList;
 let homeList=[];
+
+const heroCard = document.querySelector('.hero__card');
+
+
 fetch('https://swapi.dev/api/people/')
   .then(response => response.json())
   .then((data) => {
     dataList = data.results;
     //findPlanet();
     render();
-    })
+    });
     
     
    
@@ -53,7 +57,7 @@ function render(){
                     <div class="info__area">
                         <div class="info__title-block">
                             <div class="name__title answer-info">${warrior.name}</div> 
-                            <div class="toggle__block" id="toggle__block${idx}" onclick="toggleCheck(${idx})">
+                            <div class="toggle__block" onclick="toggleCheck(${idx})">  
                                <div class="toggle__circle" id="toggle__circle${idx}">
                                     <div class="toggle__info">i</div>
                                 </div>
@@ -75,33 +79,51 @@ function render(){
 
 
 
-
+// функция при клике на переключатель двигает ползунок вправо и открывает страницу героя
 function toggleCheck(idx){
-    const toggleCircle = document.getElementById('toggle__circle' + idx)
-    toggleCircle.classList.add('toggle__circle-check')
-    openCloseCard(idx)
+    const toggleCircle = document.getElementById('toggle__circle' + idx); // определем по какому элементу кликнули
+    toggleCircle.classList.add('toggle__circle-check');  
+    openCloseCard(idx);   // вызываем функцию открытия карточки
 }
 
-const heroCard = document.querySelector('.hero__card')
+//функция открытия карточки
 function openCloseCard(idx){
     heroCard.classList.add('hero__card-show')
-    
-    const arrowBack = document.getElementById('arrowBack')
-    arrowBack.onclick = () =>{
-        console.log('hi')
+    showHeroInfo(idx)                                       //вызываем функцию отрисовки данных героя
+    const arrowBack = document.getElementById('arrowBack')   // стрелочка закрытия карточки
+    arrowBack.onclick = () =>{                                // при клике по ней закрываем карточку
         heroCard.classList.remove('hero__card-show')
         const toggleCircle = document.getElementById('toggle__circle' + idx)
-        toggleCircle.classList.remove('toggle__circle-check')
+        toggleCircle.classList.remove('toggle__circle-check')  // передвигаем тумблер влево
     }
     
 }
 
 
-    // const arrowBack = document.getElementById('arrowBack')
-    // arrowBack.onclick = () =>{
-    //     console.log('hi')}
-      /*
-        heroCard.classList.remove('hero__card-show')
-        const toggleCircle = document.getElementById('toggle__circle' + idx)
-        toggleCircle.classList.remove('toggle__circle-check')
-        */
+// функция заполняем карточку героя
+function showHeroInfo(idx){
+    const heroName = document.querySelector('.hero__title-info');  
+    heroName.textContent = dataList[idx].name;
+
+    const heroBirthday = document.querySelector('.year__info');
+    heroBirthday.textContent = dataList[idx].birth_year;
+
+    const heroHome = document.querySelector('.home__info');
+    heroHome.textContent = dataList[idx].homeworld;
+
+    const heroEye = document.querySelector('.eye__info');
+    heroEye.textContent = dataList[idx].eye_color;
+
+    const heroGender = document.querySelector('.gender__type');
+    heroGender.textContent = dataList[idx].gender;
+
+    const heroHair = document.querySelector('.hair__info');
+    heroHair.textContent = dataList[idx].hair_color;
+
+    const heroMass = document.querySelector('.mass__value');
+    heroMass.textContent = dataList[idx].mass;
+
+    const heroSkin = document.querySelector('.skin__type');
+    heroSkin.textContent = dataList[idx].skin_color;
+}
+
