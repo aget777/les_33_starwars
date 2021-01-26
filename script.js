a177129d5e0ce1b700eedc475630a6323be453fa
Alex <am2invest@yandex.ru>
2021-01-26 19:19:18 +0500

    добавил изображения героев

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 
 let dataList;
-let homeList=[];
-
+let homeList = [];
+let allData;
 const heroCard = document.querySelector('.hero__card');
+let currentPage = 'https://swapi.dev/api/people/';
 
-
-fetch('https://swapi.dev/api/people/')
-  .then(response => response.json())
-  .then((data) => {
-    dataList = data.results;
-    //findPlanet();
-    render();
+fetch(currentPage)
+    .then(response => response.json())
+    .then((data) => {
+        dataList = data.results;
+        allData = data;
+        //findPlanet();
+        console.log(allData)
+        console.log(currentPage)
+        render();
+     //   changePage()
+        
     });
+
+let testPage = fetch(currentPage)
+.then(response => response.json())
+.then((data) => {
+    testPage = data.next
+    console.log(testPage)
+ //   changePage()
     
-    
-   
-    
+});
+console.log(testPage)
+
 // function findPlanet(){
 //     dataList.map(function(hero){
 //         fetch(hero.homeworld)
@@ -28,28 +40,28 @@ fetch('https://swapi.dev/api/people/')
 //        // console.log(homeList)
 
 //     })
-    
+
 // }
 // console.log(homeList)
 
-function render(){
+function render() {
     const warriorsList = document.querySelector('.warriors__list');
-    warriorsList.innerHTML = dataList.map(function(warrior, idx){
+    warriorsList.innerHTML = dataList.map(function (warrior, idx) {
 
 
-      console.log(warrior.homeworld)
+        //console.log(warrior.homeworld)
         fetch(warrior.homeworld)
-        .then(response => response.json())
-        .then((home) =>{
-            homeList.push(home.name);
+            .then(response => response.json())
+            .then((home) => {
+                homeList.push(home.name);
 
-        })
-      
-     //  console.log(homeList)
- 
-      //  ${fetch(warrior.homeworld).then(response => response.json()).then((home) => (home.name))}
-      
-      
+            })
+
+        //  console.log(homeList)
+
+        //  ${fetch(warrior.homeworld).then(response => response.json()).then((home) => (home.name))}
+
+
 
         return `
         <li class="hero">
@@ -75,34 +87,34 @@ function render(){
             </li>
         `
     }).join('');
-}    
+}
 
 
 
 // функция при клике на переключатель двигает ползунок вправо и открывает страницу героя
-function toggleCheck(idx){
+function toggleCheck(idx) {
     const toggleCircle = document.getElementById('toggle__circle' + idx); // определем по какому элементу кликнули
-    toggleCircle.classList.add('toggle__circle-check');  
+    toggleCircle.classList.add('toggle__circle-check');
     openCloseCard(idx);   // вызываем функцию открытия карточки
 }
 
 //функция открытия карточки
-function openCloseCard(idx){
+function openCloseCard(idx) {
     heroCard.classList.add('hero__card-show')
     showHeroInfo(idx)                                       //вызываем функцию отрисовки данных героя
     const arrowBack = document.getElementById('arrowBack')   // стрелочка закрытия карточки
-    arrowBack.onclick = () =>{                                // при клике по ней закрываем карточку
+    arrowBack.onclick = () => {                                // при клике по ней закрываем карточку
         heroCard.classList.remove('hero__card-show')
         const toggleCircle = document.getElementById('toggle__circle' + idx)
         toggleCircle.classList.remove('toggle__circle-check')  // передвигаем тумблер влево
     }
-    
+
 }
 
 
 // функция заполняем карточку героя
-function showHeroInfo(idx){
-    const heroName = document.querySelector('.hero__title-info');  
+function showHeroInfo(idx) {
+    const heroName = document.querySelector('.hero__title-info');
     heroName.textContent = dataList[idx].name;
 
     const heroBirthday = document.querySelector('.year__info');
@@ -125,5 +137,48 @@ function showHeroInfo(idx){
 
     const heroSkin = document.querySelector('.skin__type');
     heroSkin.textContent = dataList[idx].skin_color;
+
+    const heroPic = document.querySelector('.hero__pic');
+    heroPic.style.backgroundImage = 'url('+picBank[idx]+')';
+    console.log(picBank[idx])
 }
+
+
+// function changePage() {
+//     const dataNext = allData.next
+//     //console.log(dataNext)
+//     const nextPage = document.getElementById('arrowRight')
+//     nextPage.onclick = () => {
+//         currentPage = dataNext;
+//         console.log('hi')
+//     }
+// }
+
+
+
+
+
+
+const prevPage = document.getElementById('arrowLeft')
+prevPage.onclick = () => {
+    console.log('bye')
+}
+
+// const nextPage = document.getElementById('arrowRight')
+// nextPage.onclick = () => {
+//     console.log('hello')
+// }
+
+const picBank = [
+    'img/luke_skywalker.jpg',
+    'img/C_3PO.jpg',
+    'img/r2d2.jpg',
+    'img/Darth_Vader.jpg',
+    'img/Leia_Organa.jpg',
+    'img/Owen_Lars.jpg',
+    'img/Beru_Whitesun_lars.jpg',
+    'img/R5_D4.jpg',
+    'img/Biggs_Darklighter.jpg',
+    'img/Obi_Wan_Kenobi.jpg'
+]
 
